d67ec1af5c04af36b49d7d07908cf0835657b98c
Kiana Blaine <kiana.blaine1@gmail.com>
2019-02-10 01:13:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,44 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase()) 
 }
 
-
-
-function sayHiToGrandma(string) {
-  var uppercase = "HELLO!"
-  var lowercase = 'hello!'
-  var mixedCase = 'Hi there!'
-  var quiet = "I can't hear you!"
-  var loud = "
-  else if(lowercase.toLowerCase === lowercase) // true
-  {
-    return quiet
-  }
- 
-  if(love = string) // false
-  {
-    return back
-  }
-
-
- 
-
-=======
-    return loud;
-  }
-
-  if(lowercase.toLowerCase === string) // true
-  {
-    return lowercase;
-  }
- 
-  if (love === string)
-  {
-  return back
-  }
-
- 
-mixedCase.toLowerCase() === mixedCase // false
->>>>>>> 200296a56accb3bab4a0ea611810f7e4056e66be
- 
-mixedCase.toUpperCase() === mixedCase // false
+function sayHiToGrandma(string)
+{
+  
 }
