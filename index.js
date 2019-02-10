4048de6c713829fe011040c1e5647a76ec8e663e
Kiana Blaine <kiana.blaine1@gmail.com>
2019-02-10 01:13:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,15 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string)
 {
-  
+  var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
 }
